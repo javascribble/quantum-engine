235a56d73906ffddb604aca429de4d51a1a500c7
Daniel Scroggins <daniel.scroggins@mail.com>
2020-03-07 10:02:51 -0500

    simplify ecs api

--- source/architecture/ecs.js
+++ source/architecture/ecs.js
@@ -1,44 +1,30 @@
 import { createAssignPropertyTrap, createDefinePropertyTrap, createDeletePropertyTrap } from '../utilities/proxies';
 
-const systemComponents = new Map();
-const entitySystems = new Map();
+export const componentSystems = new Map();
 const componentObserver = {
     ...createAssignPropertyTrap(addComponent),
     ...createDefinePropertyTrap(addComponent),
     ...createDeletePropertyTrap(deleteComponent)
 };
 
-export function registerSystem(system, components) {
-    systemComponents.set(system, components);
-}
-
 export function createEntity() {
-    let entity = new Proxy({}, componentObserver);
-    entitySystems.set(entity, new Set());
-    return entity;
+    return new Proxy({}, componentObserver);
 }
 
 export function deleteEntity(entity) {
-    for (const system of entitySystems.get(entity)) {
-        system.delete(entity);
+    for (const component in entity) {
+        deleteComponent(entity, component);
     }
-
-    entitySystems.delete(entity);
 }
 
 function addComponent(entity, component) {
-    for (const [system, components] of systemComponents) {
-        // Entity components intersect.
+    for (const system of componentSystems.get(component)) {
+        system.add(entity);
     }
 }
 
 function deleteComponent(entity, component) {
-    let systems = entitySystems.get(entity);
-    for (const system of systems) {
-        let components = systemComponents.get(system);
-        if (components.contains(component)) {
-            system.delete(entity);
-            systems.delete(system);
-        }
+    for (const system of componentSystems.get(component)) {
+        system.delete(entity);
     }
 }
