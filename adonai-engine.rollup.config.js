7ae1337d3aa96e35091462ffdf378ff2d216d415
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-03-16 14:36:33 -0400

    remove unused packages

--- adonai-engine.rollup.config.js
+++ adonai-engine.rollup.config.js
@@ -1,24 +1,7 @@
-import { terser } from "rollup-plugin-terser";
-
-const defaults = {
-	name: 'engine',
-	format: 'es'
-};
-
 export default {
 	input: './source/main.js',
-	output: [
-		{
-			...defaults,
-			file: './build/adonai-engine.js',
-			plugins: []
-		},	
-		{
-			...defaults,
-			file: './build/adonai-engine.min.js',
-			plugins: [
-				terser()
-			]
-		}
-	]
+	output: {
+		format: 'es',		
+		file: './build/adonai-engine.js'
+	}
 };
