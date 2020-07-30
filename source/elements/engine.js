fddd21c452227f49862511bdf699607cc68502f7
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-07-30 10:17:04 -0400

    test out event interface

--- source/elements/engine.js
+++ source/elements/engine.js
@@ -1,26 +1,35 @@
 import html from '../templates/engine.js';
 
 export class Engine extends quantum.Component {
-    events = new Map();
+    #events = new Map();
 
     static template = quantum.template(html);
 
     publish(topic, value) {
-        for (const subscriber of this.events.get(topic)) {
-            subscriber(value);
+        if (this.#events.has(topic)) {
+            for (const subscriber of this.#events.get(topic)) {
+                subscriber(value);
+            }
         }
     }
 
     subscribe(topic, subscriber) {
-        if (this.events.has(topic)) {
-            this.events.get(topic).add(subscriber);
+        if (this.#events.has(topic)) {
+            this.#events.get(topic).add(subscriber);
         } else {
-            this.events.set(topic, new Set([subscriber]));
+            this.#events.set(topic, new Set([subscriber]));
         }
     }
 
     unsubscribe(topic, subscriber) {
-        this.events.get(topic).delete(subscriber);
+        if (this.#events.has(topic)) {
+            const set = this.#events.get(topic);
+            if (set.size === 1) {
+                this.#events.delete(topic);
+            } else {
+                set.delete(subscriber);
+            }
+        }
     }
 }
 
