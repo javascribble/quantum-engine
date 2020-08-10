31ccc9c19c2babff524328811950f1a9b9b92d7d
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-08-10 09:58:57 -0400

    little guy moving!

--- test/debug.js
+++ test/debug.js
@@ -3,6 +3,7 @@ import '/node_modules/@javascribble/quantum-loader/source/main.js';
 import '/node_modules/@javascribble/quantum-canvas/source/main.js';
 import '/node_modules/@javascribble/quantum-keyboard/source/main.js';
 import '/source/main.js';
+import '/test/game.js';
 
 const engine = document.querySelector('quantum-engine');
 engine.onload = () => document.body.style.visibility = 'visible';
