61798ec0eb380b7daba859c61da22608ac30444b
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-10-13 09:05:18 -0400

    working sample abstracted tilemap

--- test/debug.js
+++ test/debug.js
@@ -2,7 +2,25 @@ import '/node_modules/@javascribble/quantum/source/main.js';
 import '/node_modules/@javascribble/quantum-loader/source/main.js';
 import '/node_modules/@javascribble/quantum-canvas/source/main.js';
 import '/node_modules/@javascribble/quantum-keyboard/source/main.js';
+import '/node_modules/@javascribble/quantum-tiles/source/main.js';
 import '/source/main.js';
 
 const engine = document.querySelector('quantum-engine');
-engine.onload = api => document.body.style.visibility = 'visible';
+engine.onload = api => {
+    api.loadMany(api.options.resources.map(resource => `${api.options.resourcePath}/${resource}`), console.log).then(resources => {
+        const sprite = { ...api.options.sprites[0], image: resources[0] };
+
+        api.broker.subscribe('MoveUp', _ => sprite.dy -= 10);
+        api.broker.subscribe('MoveDown', _ => sprite.dy += 10);
+        api.broker.subscribe('MoveLeft', _ => sprite.dx -= 10);
+        api.broker.subscribe('MoveRight', _ => sprite.dx += 10);
+
+        quantum.animate(() => {
+            api.drawTiles(api.options.map, resources);
+            api.drawSprite(sprite);
+            return true;
+        });
+    });
+
+    document.body.style.visibility = 'visible';
+};
