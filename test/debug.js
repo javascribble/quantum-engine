b2adbe9dd9da1b1b31e7fd3a1e6ef83da01c5527
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-10-15 10:21:00 -0400

    merge repositories

--- test/debug.js
+++ test/debug.js
@@ -1,11 +1,11 @@
 import '/node_modules/@javascribble/quantum/source/global.js';
-import '/node_modules/@javascribble/quantum-animation/source/main.js';
-import '/node_modules/@javascribble/quantum-canvas/source/main.js';
-import '/node_modules/@javascribble/quantum-loader/source/main.js';
-import '/node_modules/@javascribble/quantum-keyboard/source/main.js';
-import '/node_modules/@javascribble/quantum-scene/source/main.js';
-import '/node_modules/@javascribble/quantum-tiles/source/main.js';
-import '/source/main.js';
+import '/node_modules/@javascribble/quantum-animation/source/global.js';
+import '/node_modules/@javascribble/quantum-canvas/source/global.js';
+import '/node_modules/@javascribble/quantum-loader/source/global.js';
+import '/node_modules/@javascribble/quantum-keyboard/source/global.js';
+import '/node_modules/@javascribble/quantum-scene/source/global.js';
+import '/node_modules/@javascribble/quantum-tiles/source/global.js';
+import '/source/global.js';
 import '/test/game.js';
 
 document.body.style.visibility = 'visible';
