d4e3d3401eb95e0fd59a9e73702e179d7c5815cf
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-10-19 12:54:02 -0400

    remove dead packages

--- test/debug.js
+++ test/debug.js
@@ -1,10 +1,7 @@
 import '/node_modules/@javascribble/quantum/source/global.js';
-import '/node_modules/@javascribble/quantum-animation/source/global.js';
 import '/node_modules/@javascribble/quantum-canvas/source/global.js';
 import '/node_modules/@javascribble/quantum-loader/source/global.js';
 import '/node_modules/@javascribble/quantum-keyboard/source/global.js';
-import '/node_modules/@javascribble/quantum-scene/source/global.js';
-import '/node_modules/@javascribble/quantum-tiles/source/global.js';
 import '/source/global.js';
 import '/test/game.js';
 
