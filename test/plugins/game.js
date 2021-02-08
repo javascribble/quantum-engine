31ec911eeab1677383e60ef8115bbe3236ae7cce
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-02-08 10:12:10 -0500

    add reset button

--- test/plugins/game.js
+++ test/plugins/game.js
@@ -1,8 +1,14 @@
-Quantum.Engine.plugins.add(api => {
+Quantum.Engine.plugins.add((api, engine) => {
     api.attachSystem({
         validate: entity => 'children' in entity,
         add: entity => {
-            entity.children.forEach(api.attachEntity)
+            entity.children.forEach(api.attachEntity);
+
+            engine.querySelector('button').addEventListener('click', event => {
+                const player = entity.children[1];
+                api.detachEntity(player);
+                api.attachEntity(player);
+            });
         }
     });
 }); 
--- test/plugins/tile.js
+++ test/plugins/tile.js
@@ -1,23 +1,25 @@
-const importUniformSpritesheet = (source, sw, sh = sw) => {
-    const sprites = [];
-    for (let row = 0; row < source.height / sh; row++) {
-        for (let column = 0; column < source.width / sw; column++) {
-            sprites.push({ source, sx: column * sh, sy: row * sh, sw, sh });
-        }
-    }
-
-    return sprites;
-};
-
 Quantum.Engine.plugins.add(api => {
     api.attachSystem({
         validate: entity => 'tileset' in entity,
         add: entity => {
-            const { tileset, indices, divisor } = entity;
+            const { tileset, divisor } = entity;
             const { sheet, size } = tileset;
 
+            const sprites = [];
+            for (let row = 0; row < sheet.height / size; row++) {
+                for (let column = 0; column < sheet.width / size; column++) {
+                    sprites.push({ source: sheet, sx: column * size, sy: row * size, sw: size, sh: size });
+                }
+            }
+
+            const indices = [];
+            for (let i = 0; i < divisor; i++) {
+                for (let ii = 0; ii < divisor; ii++) {
+                    indices.push(Math.round(Math.random()));
+                }
+            }
+
             const children = [];
-            const sprites = importUniformSpritesheet(sheet, size);
             for (let index = 0; index < indices.length; index++) {
                 const tile = { ...sprites[indices[index]] };
                 tile.dx = tile.sw * (index % divisor);
