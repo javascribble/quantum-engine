f76a5b68dc0334aab592e3bea42a051aac1171e5
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-03-17 12:58:07 -0400

    add jpg

--- source/utilities/loaders.js
+++ source/utilities/loaders.js
@@ -1,8 +1,11 @@
 const { loaders } = quantum;
 
-loaders.png = (url, options) => new Promise((resolve, reject) => {
+const loadImage = (url, options) => new Promise((resolve, reject) => {
     const image = new Image();
     image.onload = () => resolve(image);
     image.onerror = () => reject(image);
     image.src = url;
 });
+
+loaders.png = loadImage;
+loaders.jpg = loadImage;
