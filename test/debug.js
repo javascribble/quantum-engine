d441a2a63a1d5f60c78de64860d564e2fd51fcff
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-06-09 12:25:28 -0400

    change preset name

--- test/debug.js
+++ test/debug.js
@@ -3,7 +3,7 @@ import '/node_modules/@javascribble/quantum-canvas/source/main.js';
 import '/node_modules/@javascribble/quantum-canvas/source/decorators/loaders.js';
 import '/node_modules/@javascribble/quantum-canvas/source/extensions/draw.js';
 import '/node_modules/@javascribble/quantum-input/source/main.js';
-import '/source/presets/engine.js';
+import '/source/presets/standard.js';
 import '/source/main.js';
 import '/test/game.js';
 
