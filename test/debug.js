39387136198805326bb264fa75afd65c4009965d
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-08 12:58:21 -0400

    update test

--- test/debug.js
+++ test/debug.js
@@ -1,10 +1,14 @@
 import '/node_modules/@javascribble/quantum/source/main.js';
 import '/node_modules/@javascribble/quantum-canvas/source/main.js';
-import '/node_modules/@javascribble/quantum-canvas/source/decorators/loaders.js';
 import '/node_modules/@javascribble/quantum-canvas/source/extensions/draw.js';
+import '/node_modules/@javascribble/quantum-canvas/source/plugins/loaders.js';
 import '/node_modules/@javascribble/quantum-input/source/main.js';
-import '/source/presets/standard.js';
+import '/node_modules/@javascribble/quantum-input/source/plugins/keyboard.js';
 import '/source/main.js';
+import '/source/extensions/prototypes.js';
+import '/source/extensions/resources.js';
+import '/source/plugins/animation.js';
+import '/source/plugins/architecture.js';
 import '/test/game.js';
 
 document.body.style.visibility = 'visible';
